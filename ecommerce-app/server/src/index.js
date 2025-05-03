4d299c13db4932e41873b231f59f0caac5aab6f4
Saad Ali <a.saadmughal57@gmail.com>
2025-05-03 23:18:42 +0500

    renamed this and used module js

--- ecommerce-app/server/src/index.js
+++ ecommerce-app/server/src/index.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
 
 dotenv.config();
 const app = express();
@@ -9,15 +10,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
 app.get('/', (req, res) => {
-  res.send('E-commerce API');
+  res.send('Hello E-commerce API');
 });
 
 const PORT = process.env.PORT || 5000;
